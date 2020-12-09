5f145cb88ceb572520c08821ad9e71f9ade3ad71
jose <joseluis19963@gmail.com>
2020-12-09 14:23:38 -0500

    getUsers method

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var bcrypt = require('bcrypt-nodejs');
+var mongoosePaginate = require('mongoose-pagination');
 var User = require('../models/user');
 var jwt = require('../services/jwt');
 
@@ -115,11 +116,36 @@ function getUser(req, res) {
     });
 }
 
+// Devolver un listado de usuarios paginados
+function getUsers(req, res) {
+    var identity_user_id = req.user.sub;
+
+    var page = 1;
+    if (req.params.page) {
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 5;
+
+    User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) => {
+        if (err) return res.status(500).send({ message: 'Error en la petición.' });
+
+        if (!users) return res.status(404).send({ message: 'No hy usuarios disponibles.' });
+
+        return res.status(200).send({ 
+            users,
+            total,
+            pages: Math.ceil(total / itemsPerPage) 
+        });
+    });
+}
+
 // Exportacion de metodos
 module.exports = {
     home,
     pruebas,
     saveUser,
     loginUser,
-    getUser
+    getUser,
+    getUsers
 }
