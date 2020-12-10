66e6627d653b74216ab8c1c8cec9e7d2a3f12034
jose <joseluis19963@gmail.com>
2020-12-09 20:35:03 -0500

    deleteFollow method

--- controllers/follow.js
+++ controllers/follow.js
@@ -17,10 +17,21 @@ function saveFollow(req, res) {
         if (!followStored) return res.status(404).send({ message: 'EL seguimiento no se ha guardado.' });
 
         return res.status(200).send({ follow: followStored });
-    })
+    });
+}
 
+function deleteFollow(req, res) {
+    var userId = req.user.sub;
+    var followId = req.params.id;
+
+    Follow.find({ 'user': userId, 'followed': followId }).remove(err => {
+        if (err) return res.status(500).send({ message: 'Error al dejar de seguir.' });
+
+        return res.status(200).send({ message: 'El follow se ha eliminado' });
+    });
 }
 
 module.exports = {
-    saveFollow
+    saveFollow,
+    deleteFollow
 }
