b03ebc00fdd496fd92b9eb1752c5318c74a525a7
JL-OLEMAR <joseluis19963@gmail.com>
2021-01-04 03:31:37 -0500

    updated getReceivedMessages and getEmitMessages methods

--- controllers/message.js
+++ controllers/message.js
@@ -37,7 +37,7 @@ function getReceivedMessages(req, res) {
     }
     var itemsPerPage = 4;
 
-    Message.find({ receiver: userId }).populate('emitter', 'name surname image nick _id').paginate(page, itemsPerPage, (err, messages, total) => {
+    Message.find({ receiver: userId }).populate('emitter', 'name surname image nick _id').sort('-created_at').paginate(page, itemsPerPage, (err, messages, total) => {
         if (err) return res.status(500).send({ message: 'Error en la petición.' });
         if (!messages) return res.status(404).send({ message: 'No hay mensajes.' });
         return res.status(200).send({
@@ -56,7 +56,7 @@ function getEmitMessages(req, res) {
     }
     var itemsPerPage = 4;
 
-    Message.find({ emitter: userId }).populate('emitter receiver', 'name surname image nick _id').paginate(page, itemsPerPage, (err, messages, total) => {
+    Message.find({ emitter: userId }).populate('emitter receiver', 'name surname image nick _id').sort('-created_at').paginate(page, itemsPerPage, (err, messages, total) => {
         if (err) return res.status(500).send({ message: 'Error en la petición.' });
         if (!messages) return res.status(404).send({ message: 'No hay mensajes.' });
         return res.status(200).send({
